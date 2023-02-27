5c8abd80f8e0fe8547479970addef3e9ed9fd6c7
yourfavoriteself <xeniacuzmina@yandex.ru>
2023-02-27 17:30:56 +0300

    updated hello_world.cpp to ask for name

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() { 
 string n;
-cout<<"ваше имя уважаемый:"<<endl;
+cout<<"your name, darling:"<<endl;
 cin>>n;
     cout << "Hello, World! from " << n <<endl; 
     return 0;
