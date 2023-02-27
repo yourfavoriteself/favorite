894486bf50b5aef794562b81761c5668ef2f0c1b
yourfavoriteself <113239587+yourfavoriteself@users.noreply.github.com>
2023-02-27 20:55:14 +0300

    Update hello.cpp
    new comment

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@
 int
 main()
 {
-  std::string n; // подписывайтесь на @yourfavoriteself
+  std::string n; // подписывайтесь аккаунт на @yourfavoriteself
   std::cout << "your name, darling:" << std::endl;
   std::cin >> n;
   std::cout << "Hello, World! from " << n << std::endl;
