aea2defa27100e73214d2e7be9ccc45823e45166
yourfavoriteself <xeniacuzmina@yandex.ru>
2023-02-27 20:22:52 +0300

    changed codestyle

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-//using namespace std;
+// using namespace std;
 
-int main() { 
-std::string n;//подписывайтесь на @yourfavoriteself
-std::cout<<"your name, darling:"<<std::endl;
-std::cin>>n;
-std::cout << "Hello, World! from " << n <<std::endl; 
-    return 0;
+int
+main()
+{
+  std::string n; // подписывайтесь на @yourfavoriteself
+  std::cout << "your name, darling:" << std::endl;
+  std::cin >> n;
+  std::cout << "Hello, World! from " << n << std::endl;
+  return 0;
 }
