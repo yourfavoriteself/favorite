1f433eec5bce0b98dfd91249f20be5988b6be5ba
yourfavoriteself <xeniacuzmina@yandex.ru>
2023-02-27 17:27:21 +0300

    added hello_world.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { 
+string n;
+cout<<"ваше имя уважаемый:"<<endl;
+cin>>n;
+    cout << "Hello, World! from " << n <<endl; 
+    return 0;
+}
